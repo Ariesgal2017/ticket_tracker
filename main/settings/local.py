553cb7e534dcdb17edcd96cddcc85bb4fe948fee
Adam Cottrill <racottrill@gmail.com>
2018-10-24 18:07:52 -0400

    Stashing old updates.
    Not sure how old these changes are. Need to commit them to pull from
    the work repository.

--- main/settings/local.py
+++ main/settings/local.py
@@ -8,16 +8,16 @@ DATABASES = {
 }
 
 
-MIDDLEWARE_CLASSES += (
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
-)
-
+#MIDDLEWARE_CLASSES += (
+#    'debug_toolbar.middleware.DebugToolbarMiddleware',
+#)
+#
 INSTALLED_APPS += (
-    'debug_toolbar',
+#    'debug_toolbar',
     'django_extensions',
 )
 
-INTERNAL_IPS = ('127.0.0.1', )   #added for debug toolbar
-DEBUG_TOOLBAR_CONFIG = {
-    'INTERCEPT_REDIRECTS': False,
-}
+#INTERNAL_IPS = ('127.0.0.1', )   #added for debug toolbar
+#DEBUG_TOOLBAR_CONFIG = {
+#    'INTERCEPT_REDIRECTS': False,
+#}
